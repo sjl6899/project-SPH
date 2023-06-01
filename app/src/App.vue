619f694cbb6656cafd491319c0d0835f730a4a6f
sjl6899 <810330029@qq.com>
2023-06-01 16:23:04 +0800

    head/footer

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>我是根组件1
-
+  <div>
+    <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+//引入组件
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Header,
+    Footer,
+  }
 }
 </script>
 
-<style>
+<style scope>
 
 </style>
