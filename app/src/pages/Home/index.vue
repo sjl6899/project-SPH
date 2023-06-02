e9622d16a521e454aabff12d1d66706877d2cfc5
sjl6899 <810330029@qq.com>
2023-06-02 12:43:00 +0800

    6.2

--- app/src/pages/Home/index.vue
+++ app/src/pages/Home/index.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>我是首页</div>
+  <div>
+    <!--三级联动组件：已经注册为全局组件，因此不需要引入-->
+    <TypeNav/>
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
-  components: {
-  }
+  name: 'App'
 }
 </script>
 
