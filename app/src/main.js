a9b62059ab21afe9cdbed9687bec5f19a66a78a6
sjl6899 <810330029@qq.com>
2023-07-26 21:40:33 +0800

    全局组件-Carousel

--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 //三级联动组件，全局组件
 import TypeNav from '@/components/TypeNav'
+import Carousel from '@/components/Carousel'
 //引入路由
 import router from '@/router'
 //引入仓库
@@ -10,6 +11,7 @@ import store from './store';
 import "swiper/css/swiper.css"
 //第一个参数：全局组件名  第二个参数：哪一个组件
 Vue.component(TypeNav.name,TypeNav);
+Vue.component(Carousel.name,Carousel);
 
 Vue.config.productionTip = false
 //引入MockServe.js---mock数据
--- app/src/pages/Home/Floor/index.vue
+++ app/src/pages/Home/Floor/index.vue
@@ -22,19 +22,8 @@
               <img :src="list.imgUrl" />
             </div>
             <div class="floorBanner">
-              <div class="swiper-container" ref="floor1Swiper">
-                <div class="swiper-wrapper">
-                  <div class="swiper-slide" v-for="(carousel) in list.carouselList" :key="carousel.id">
-                    <img :src="carousel.imgUrl" />
-                  </div>
-                </div>
-                <!-- 如果需要分页器 -->
-                <div class="swiper-pagination"></div>
-
-                <!-- 如果需要导航按钮 -->
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
-              </div>
+              <!-- 全局组件：Carousel轮播图 -->
+              <Carousel :list="list.carouselList"/>
             </div>
             <div class="split">
               <span class="floor-x-line"></span>
@@ -65,24 +54,11 @@
 </template>
 
 <script>
-import Swiper from 'swiper'
+
 export default {
   name: "Floor",
   props:['list'],
-  mounted(){
-    var mySwiper = new Swiper(this.$refs.floor1Swiper, {
-        loop: true,
-        // 如果需要分页器
-        pagination: {
-          el: ".swiper-pagination",
-          clickable:true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
-  }
+  
 };
 </script>
 
